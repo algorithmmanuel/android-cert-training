9170b67e98a584d4aec5bcc658b4f284b9a80d21
algorithmmanuel <emmanuelasuquo1991@gmail.com>
2021-01-04 16:58:24 +0100

    Add files via upload

---
+++ BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.pluralsight.notifyme;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.pluralsight.notifyme";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
